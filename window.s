f85c6550324c08d896c6bd3e6b6c71aaa62eb916
Philippe Gaultier <philippe.gaultier@ory.sh>
2026-09-18 14:38:04 +0200

    send msg to get NsApplication instance

--- window.s
+++ window.s
@@ -11,6 +11,7 @@
 // STR_NSString: .asciz "NSString"
 
 NSApplication.str: .asciz "NSApplication"
+sharedApplication.str: .asciz "sharedApplication"
 UNKNOWN_ERROR.str: .asciz "unknown error"
 OK.str: .asciz "ok"
 
@@ -22,31 +23,46 @@ OK.str: .asciz "ok"
 
 .global _main
 _main:
-	sub sp, sp, #0x20 ; FIXME: real stack size.
-	stp x29, x30, [sp, #0x10]  ; save frame pointer + return address
-	add x29, sp, #0x10         ; establish new frame pointer
+	sub sp, sp, #0x30 ; 3 locals (24B, rounded to 32) + 16B saved pair
+	stp x29, x30, [sp, #0x20]  ; save frame pointer + return address
+	add x29, sp, #0x20         ; establish new frame pointer
 
 	; Locate `NSApplication` class and store it in `[sp]`.
 	LEA x0, NSApplication.str
 	bl _objc_getClass
-	str x0, [sp]
 	cmp x0, 0
 	b.eq .die
+	str x0, [sp]
+
+	; Register `sharedApplication` and store it in `[sp + 8]`.
+	LEA x0, sharedApplication.str
+	bl _sel_registerName
+	cmp x0, 0
+	b.eq .die
+	str x0, [sp, 8]
+
+	; Send the message.
+	ldr x0, [sp]
+	ldr x1, [sp, 8]
+	bl _objc_msgSend
+	cmp x0, 0
+	b.eq .die
+	str x0, [sp, 16]
 
 
 	LEA x0, OK.str
-	b _puts
+	bl _puts
 
 	mov w0, #0
-	ldp x29, x30, [sp, #0x10] ; restore frame pointer and return address.
-	add sp, sp, #0x20 ; reset frame pointer.
+	ldp x29, x30, [sp, #0x20] ; restore frame pointer and return address.
+	add sp, sp, #0x30 ; reset frame pointer.
 	ret
 
 
 .die:
 	LEA x0, UNKNOWN_ERROR.str
-	b _puts
+	bl _puts
 	mov w0, #1
-	ldp x29, x30, [sp, #0x10] ; restore frame pointer and return address.
-	add sp, sp, #0x20 ; reset frame pointer.
+	ldp x29, x30, [sp, #0x20] ; restore frame pointer and return address.
+	add sp, sp, #0x30 ; reset frame pointer.
 	ret
